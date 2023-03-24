eb2f53f551b25a7d9910264a6ef127cbe9339a1c
Hyunwoo Kim <32376551+KevinError@users.noreply.github.com>
2023-03-24 02:20:46 -0700

    factory update

--- CreationalPattern/src/BuilderMain.java
+++ CreationalPattern/src/BuilderMain.java
@@ -6,20 +6,39 @@ public class BuilderMain {
         Pizza pizzaHut = new Pizza.PizzaBuilder(
                 "PizzaHut",
                 "Small",
-                new String[]{"Pepperoni", "Peppers", "Ham"}
+                new String[]{"Pepperoni", "Peppers"}
         ).build();
         Pizza pizzaHut2 = new Pizza.PizzaBuilder(
                 "PizzaHut",
-                "Medium",
+                "Large",
+                new String[]{"Pepperoni", "Peppers", "Ham"}
+        ).build();
+        Pizza littleCeasars = new Pizza.PizzaBuilder(
+                "littleCeasars",
+                "Small",
                 new String[]{"Pepperoni", "Peppers", "Ham", "Chicken", "Olives", "Pesto"}
         ).build();
-        Pizza pizzaHut3 = new Pizza.PizzaBuilder(
-                "PizzaHut",
-                "Large",
-                new String[]{"Pepperoni", "Peppers", "Ham", "Chicken", "Olives", "Pesto", "Extra Cheese", "Beef", "Onions"}
+        Pizza littleCeasars2 = new Pizza.PizzaBuilder(
+                "littleCeasars",
+                "Medium",
+                new String[]{"Pepperoni", "Peppers", "Ham", "Chicken", "Olives", "Pesto", "Extra Cheese", "Beef"}
         ).build();
+        Pizza Dominos = new Pizza.PizzaBuilder(
+                "Dominos",
+                "Small",
+                new String[]{"Pepperoni"}
+        ).build();
+        Pizza Dominos2 = new Pizza.PizzaBuilder(
+                "Dominos",
+                "Large",
+                new String[]{"Pepperoni", "Peppers", "Ham"}
+        ).build();
+
         System.out.println(pizzaHut.eat());
         System.out.println(pizzaHut2.eat());
-        System.out.println(pizzaHut3.eat());
+        System.out.println(littleCeasars.eat());
+        System.out.println(littleCeasars2.eat());
+        System.out.println(Dominos.eat());
+        System.out.println(Dominos2.eat());
     }
 }
